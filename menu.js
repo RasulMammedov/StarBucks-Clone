d7476047604db2e914a1f31a0ae513a9c5ce7e3c
RasulMammedov <rasulmammedov77@gmail.com>
2025-04-28 03:18:12 +0400

    fix: responsive menu

--- menu.js
+++ menu.js
@@ -1,9 +1,10 @@
 const btn = document.getElementById('menu')
 const nav = document.getElementById('nav')
 const menu = document.getElementById('openMenu')
+const div = document.getElementById('bg-grad')
 const body = document.querySelector('body')
 
 btn.onclick = function() {
     menu.classList.toggle('open')
-    body.classList.toggle('bg-[#0009]')
+    div.classList.toggle('bg-[#0009]')
 }
