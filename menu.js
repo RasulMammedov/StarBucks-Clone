e45c4687cfbb1d6a66bd29731f2e9bd918b84b3b
RasulMammedov <rasulmammedov77@gmail.com>
2025-04-28 11:24:02 +0400

    Complete main functionality

--- menu.js
+++ menu.js
@@ -4,7 +4,19 @@ const menu = document.getElementById('openMenu')
 const div = document.getElementById('bg-grad')
 const body = document.querySelector('body')
 
-btn.onclick = function() {
+
+/* btn.onclick = function() {
     menu.classList.toggle('open')
     div.classList.toggle('bg-[#0009]')
+    body.classList.toggle('max-md:overflow-hidden')
+} */
+
+function toggleMenu() {
+    menu.classList.toggle('open')
+    div.classList.toggle('bg-[#0009]')
+    body.classList.toggle('max-md:overflow-hidden')
+}
+
+window.onresize = function() {
+    if (menu.classList.contains('open') && window.innerWidth > 760) toggleMenu()
 }
